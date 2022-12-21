9930e9f44fb1c0358331c46b5ba2091dd1a3bbee
lucas.fanech <fanech.lucas@gmail.com>
2022-12-21 23:08:01 +0100

    Fix bug

--- routes/routes.php
+++ routes/routes.php
@@ -30,44 +30,50 @@ Flight::route('GET /success', function(){
 
 Flight::route('POST /register', function() {
     include('db.php');
+    $valid = true;
     // Récupérez les données du formulaire
-    $name = Flight::request()->data['name'];
+    $name = Flight::request()->data['username'];
     $email = Flight::request()->data['email'];
     $password = Flight::request()->data['password'];
-
-    // Vérifiez si les champs sont remplis
-    if (empty($name) || empty($email) || empty($password)) {
-        Flight::redirect('/register');
+    // [ Conditions de validation ]
+    // Vérifiez si les champs sont vides
+    if ($name == '' || $email == '' || $password == '') {
+        $valid = false;
     }
 
-    // Vérifiez si l'adresse email est valide
+    // Vérifiez si l'adresse e-mail est valide
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        Flight::redirect('/register');
+        $valid = false;
     }
 
-    // Vérifiez si l'adresse email existe déjà
-    $sql = "SELECT * FROM utilisateur WHERE email = :email";
-    if ($stmt = $pdo->prepare($sql)) {
-        // vérifiez si l'adresse email existe déjà $email
-        $stmt->bindParam(":email", $email, PDO::PARAM_STR);
-        if ($stmt->execute()) {
-            if ($stmt->rowCount() == 1) {
-                Flight::redirect('/register');
-            }
-        }
-    }
-
-
-    // Vérifiez si la longueur du mot de passe est au moins de 8 caractères
-    if (strlen($password) < 8) {
-        Flight::redirect('/register');
-    }
-
-    // Insérez les données dans la table de la base de données
-
-    $sql = "INSERT INTO utilisateur (Nom, Email, Motdepasse) VALUES (:name, :email, :password)";
+    // Vérifiez si l'e-mail existe déjà
+    $sql = "SELECT COUNT(*) AS num FROM utilisateur WHERE email = :email";
     $stmt = $pdo->prepare($sql);
-    $stmt->execute(['name' => $name, 'email' => $email, 'password' => $password]);
-    Flight::redirect('/success');
+    $stmt->bindValue(':email', $email);
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($row['num'] > 0) {
+        $valid = false;
+    }
+
+    // Vérifier si le mot de passe fait au moins 8 caractères
+    if (strlen($password) < 8) {
+        $valid = false;
+    }
+
+    // [Redirection et requête]
+    if (!$valid) {
+        // Si tout n'est pas valide affichez un message d'erreur
+        Flight::render("register.tpl",array());
+    } else {
+        // Si les champs ne sont pas vides, insérez les données dans la base de données
+        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+        $sql = "INSERT INTO utilisateur (Nom, Email, Motdepasse) VALUES (:name, :email, :password)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['name' => $name, 'email' => $email, 'password' => $passwordHash]);
+        Flight::redirect('/success');
+    }
+
+
 });
 ?>
